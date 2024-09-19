19d05690cb3ab69d1f9ef6f269359707e7377259
Fabio Anzola <if24b001@technikum-wien.at>
2024-09-19 12:35:51 +0000

    Get option args and searchpath

--- myfind.c
+++ myfind.c
@@ -5,5 +5,36 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world");
+    int recursive = 0;
+    int case_insensitive = 0;
+    int opt;
+    char *searchpath = NULL;
+    
+    // Parse option arguments with getopt
+    while ((opt = getopt(argc, argv, "Ri")) != EOF) {
+        switch (opt) {
+            case 'R':
+                recursive = 1;
+                break;
+            case 'i':
+                case_insensitive = 1;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-R] [-i] searchpath filename1 [filename2 ...]\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    // Chech if searchpath and filenames have been entered
+    if (optind >= argc) { // optind is set by getopt
+        fprintf(stderr, "Expected searchpath and filenames\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Extract searchpath from arguments
+    searchpath = argv[optind];
+    optind++;
+
+    printf(searchpath);
+
 }
